f98270ffdda88911577a0eb0ec3ea94f6574b339
Nitzan Hen <code@nitzanhen.net>
2022-09-12 21:48:10 +0300

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import { join } from 'path';
+import { defineConfig } from 'rollup';
+import ts from 'rollup-plugin-ts';
+
+const src = join(__dirname, 'src');
+const dist = join(__dirname, 'dist');
+
+export default defineConfig([
+  {
+    input: join(src, 'index.ts'),
+    plugins: [
+      ts()
+    ],
+    output: {
+      file: join(dist, 'index.mjs'),
+      format: 'es'
+    }
+  },
+  {
+    input: join(src, 'index.ts'),
+    plugins: [
+      ts({
+        tsconfig: {
+          declaration: false
+        }
+      })
+    ],
+    output: {
+      file: join(dist, 'index.cjs'),
+      format: 'cjs'
+    }
+  }
+])
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+
+/**
+ * Can be called with a function that transforms the current value `x`, or with no arguments
+ * to return the current value.
+ */
+export interface Pipe<T> {
+  <S>(fn: (x: T) => S): Pipe<S>
+  (): T;
+}
+
+/**
+ * Can be called with a value to initiate the pipeline with,
+ * or with no value to initiate a pipeline with no value. 
+ */
+export interface PipeFactory {
+  <T>(value: T): Pipe<T>;
+  (): Pipe<never>;
+}
+
+
+/**
+ * Pipes a value through a chain of functions in succession.
+ * 
+ * To end the pipeline and get the final result, call the pipeline with no arguments
+ * 
+ * @example
+ * const v1 = pipe(1)
+ *   (x => x + 1)
+ *   (x => x * 2)
+ *   ();
+ * 
+ * console.log(v1) // 4
+ */
+export const pipe = (<T>(x: T) => {
+  return (
+    <S>(fn?: (x: T) => S) => fn ? pipe(fn(x)) : x
+  );
+}) as PipeFactory;
